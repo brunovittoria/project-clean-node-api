db81918ab3914936c431af6e23db2c9afc880351
brunovittoria <brunogun1999@gmail.com>
2025-03-21 07:59:55 -0300

    feat: ensure SignupController calls AddAccount with correct values

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,14 @@
+import { AccountModel } from '../models/account'
+
+//Criamos a interface AddAccount aqui pois somente esse useCase ira usar ela, ja o AccountModel é o modelos da conta que esta no DB e pode ser usado em outros lugares
+export interface AddAccountModel {
+  name: string
+  email: string
+  password: string
+}
+
+export interface AddAccount {
+  add (account: AddAccountModel): AccountModel
+}
+
+//Criamos essas interfaces aqui dentro e nao dentro de protocols pois no TDD as interfaces dos useCases sao criadas dentro do useCase e nao dentro de protocols
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,13 +1,16 @@
 import { HttpResponse, HttpRequest, Controller, EmailValidator } from '../protocols'
 import { MissingParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
+import { AddAccount } from '../../domain/usecases/add-account'
 
 export class  SignupController implements Controller {
 
   private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
@@ -31,6 +34,12 @@ export class  SignupController implements Controller {
       if(!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      this.addAccount.add({ 
+        name, 
+        email, 
+        password
+      })
 
       return {
         statusCode: 200,
