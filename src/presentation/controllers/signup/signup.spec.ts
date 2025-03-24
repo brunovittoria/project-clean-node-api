a7b30f12680b3bc079e88fbed8eda816bfc83efc
brunovittoria <brunogun1999@gmail.com>
2025-03-24 07:10:40 -0300

    test: ensure  signupController returns 500 if AddAccount throws

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -206,6 +206,30 @@ describe('SignupController', () => {
     expect(httpResponse.body).toEqual(new ServerError())
   })
 
+  test('Should return 500 if email validator throws', () => {
+    // Arrange (Preparação)
+    const { sut, addAccountStub } = makeSut()
+
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    // Act (Ação)
+    const httpResponse = sut.handle(httpRequest)
+
+    // Assert (Verificação)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
+
   test('Should call AddAccount with correct values', () => {
     // Arrange (Preparação)
     const { sut, addAccountStub } = makeSut()
